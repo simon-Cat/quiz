9083b2efa24bb5423fdacbd022c4890779bacd18
Bekir Murtazaev <bekirmurtazaev@yandex.ru>
2024-11-25 16:04:11 +0300

    add next/font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import "@/styles/globals.css";
+import "@/styles/globals.css"
+import { Montserrat } from "next/font/google"
+
+const montserrat = Montserrat({
+  weight: ["400", "700", "800"],
+  subsets: ["latin"],
+  display: "swap",
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={montserrat.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
