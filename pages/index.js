edc3e588178f29f7e0050693f12f67a1bed5439e
Bekir Murtazaev <bekirmurtazaev@yandex.ru>
2024-11-25 16:05:10 +0300

    add Quize component into index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head"
-import styles from "@/styles/Home.module.css"
+import { questions } from "@/public/questions"
+import Quize from "@/components/Quize/Quize"
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <Quize quizeTitle="Тестирование" questions={questions} />
     </>
   )
 }
